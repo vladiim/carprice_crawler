af14ccac9b1da87a20ba3f8ffba4f046bf7af71b
Vlad Mehakovic <vladiim@yahoo.com.au>
2015-09-13 16:07:14 +1000

    Fetches data from key pages, next steps: mongo db & pagination

--- lib/car_price.rb
+++ lib/car_price.rb
@@ -4,4 +4,31 @@ autoload(:Car,     'car_price/car')
 autoload(:Crawler, 'car_price/crawler')
 
 class CarPrice
+  CATEGORIES = %w(sedan hatch convertible coupe wagon SUV Performance Prestige)
+
+  attr_reader :host
+  def initialize
+    @host = 'http://www.redbook.com.au/'
+  end
+
+  def get
+    CATEGORIES.map do |category|
+      {
+        category: category,
+        cars: Crawler.new.cars(url(category))
+      }
+    end
+  end
+
+  private
+
+  def url(category, year = 2015)
+    if %w(Prestige Performance).include? category
+      "#{host}cars/research/new?q=%28Lifestyle%3D#{category}%26YearRange%3Drange%5B#{year}..%5D%29"
+    else
+      "#{host}cars/research/new?q=%28BodyType%3D#{category}%26YearRange%3Drange%5B#{year}..%5D%29"
+    end
+  end
 end
+
+# http://www.redbook.com.au/cars/models/lifestyle/convertible?s=15&evnt=pagination
